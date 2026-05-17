c589c3e21b579f1480b20693cce9e3d5c23fe9a5
Kenneth Koon <kennethk.dev@gmail.com>
2026-05-17 22:21:46 +1000

    feat: Implemented service for providing JWT

---
+++ src/main/java/com/synapse/backend/auth/JwtService.java
@@ -0,0 +1,42 @@
+package com.synapse.backend.auth;
+
+import java.time.Instant;
+
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.security.oauth2.jwt.JwsHeader;
+import org.springframework.stereotype.Service;
+
+import com.synapse.backend.user.User;
+
+@Service
+public class JwtService {
+    private final JwtEncoder jwtEncoder;
+    private final JwtProperties jwtProperties;
+
+    public JwtService(
+        JwtEncoder jwtEncoder,
+        JwtProperties jwtProperties
+    ) {
+        this.jwtEncoder = jwtEncoder;
+        this.jwtProperties = jwtProperties;
+    }
+
+    public String generateAccessToken(User user) {
+        Instant now = Instant.now();
+        JwsHeader headers = JwsHeader.with(MacAlgorithm.HS256).build();
+
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+            .issuer(jwtProperties.issuer())
+            .issuedAt(now)
+            .expiresAt(now.plus(jwtProperties.accessTokenTtl()))
+            .subject(user.getId().toString())
+            .claim("email", user.getEmail())
+            .claim("name", user.getName())
+            .build();
+
+        return jwtEncoder.encode(JwtEncoderParameters.from(headers, claims)).getTokenValue();
+    }
+}
