caccb9b6275cffecf3daaf88f9e861c832d54803
Kenneth Koon <kennethk.dev@gmail.com>
2026-05-17 22:19:46 +1000

    feat: Created EmailAlreadyExists exception

---
+++ src/main/java/com/synapse/backend/auth/exceptions/EmailAlreadyExistsException.java
@@ -0,0 +1,8 @@
+package com.synapse.backend.auth.exceptions;
+
+public class EmailAlreadyExistsException extends RuntimeException {
+
+    public EmailAlreadyExistsException(String email) {
+        super("Email is already registered: " + email);
+    }
+}
