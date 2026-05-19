a6b7cc7c0b6fbecd414548ddf730e4725692dbf8
Kenneth Koon <kennethk.dev@gmail.com>
2026-05-19 16:49:45 +1000

    docs: Updated Swagger to know that routes are protected

--- src/main/java/com/synapse/backend/docs/OpenApiConfig.java
+++ src/main/java/com/synapse/backend/docs/OpenApiConfig.java
@@ -3,6 +3,7 @@ package com.synapse.backend.docs;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 
 import org.springframework.context.annotation.Bean;
@@ -22,6 +23,7 @@ public class OpenApiConfig {
                 .addSecuritySchemes("bearerAuth", new SecurityScheme()
                     .type(SecurityScheme.Type.HTTP)
                     .scheme("bearer")
-                    .bearerFormat("JWT")));
+                    .bearerFormat("JWT")))
+            .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
     }
 }
