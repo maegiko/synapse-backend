00c467de148e0cb60a4d2941103eec1eb3820bb4
Kenneth Koon <kennethk.dev@gmail.com>
2026-05-19 17:29:30 +1000

    feat: Implemented user details service

--- src/main/java/com/synapse/backend/user/UserService.java
+++ src/main/java/com/synapse/backend/user/UserService.java
@@ -2,7 +2,8 @@ package com.synapse.backend.user;
 
 import org.springframework.stereotype.Service;
 
-import com.synapse.backend.user.dto.MyDetailsResponse;
+import com.synapse.backend.user.dto.UserDetailsResponse;
+import com.synapse.backend.user.exceptions.UserNotFoundException;
 
 @Service
 public class UserService {
@@ -12,7 +13,19 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public MyDetailsResponse getMyDetails(Long userId) {
-        return null;
+    /**
+     * Gets the user's details by Id.
+     * @param userId the user's Id.
+     * @return the user's full name and email.
+     * @throws UserNotFoundException if the user Id does not exist in DB.
+     */
+    public UserDetailsResponse getUserDetails(Long userId) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new UserNotFoundException(userId));
+
+        return new UserDetailsResponse(
+            user.getName(),
+            user.getEmail()
+        );
     }
+
 }
