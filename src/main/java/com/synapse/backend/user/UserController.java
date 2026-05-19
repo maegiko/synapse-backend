921a9eb74e0736c863d9eebd3301bbe80cb9cea7
Kenneth Koon <kennethk.dev@gmail.com>
2026-05-19 17:40:26 +1000

    feat: Implemented api/user/details

--- src/main/java/com/synapse/backend/user/UserController.java
+++ src/main/java/com/synapse/backend/user/UserController.java
@@ -2,12 +2,24 @@ package com.synapse.backend.user;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.synapse.backend.user.dto.UserDetailsResponse;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api/user")
+@SecurityRequirement(name = "bearerAuth")
 public class UserController {
     private final UserService userService;
 
@@ -15,9 +27,29 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/me")
-    public String getMyDetails(@RequestParam String param) {
-        return new String();
+    @GetMapping("/details")
+    @Operation(
+        summary = "Get user details",
+        description = "Gets the details of the currently logged in user.",
+        responses = {
+            @ApiResponse(responseCode = "200", description = "Got user details"),
+            @ApiResponse(
+                responseCode = "401",
+                description = "Unauthenticated user",
+                content = @Content
+            ),
+            @ApiResponse(
+                responseCode = "404",
+                description = "User not found",
+                content = @Content(schema = @Schema(implementation = com.synapse.backend.shared.ErrorResponse.class))
+            )
+        }
+    )
+    public ResponseEntity<UserDetailsResponse> getUserDetails(@AuthenticationPrincipal Jwt jwt) {
+        Long userId = Long.valueOf(jwt.getSubject());
+        UserDetailsResponse res = userService.getUserDetails(userId);
+
+        return ResponseEntity.ok(res);
     }
 
 }
