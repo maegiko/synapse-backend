a74b9a2a44093851e7e3368258d2d88148ed563c
Kenneth Koon <kennethk.dev@gmail.com>
2026-05-19 16:52:10 +1000

    docs: Reverted global security item

--- src/main/java/com/synapse/backend/docs/OpenApiConfig.java
+++ src/main/java/com/synapse/backend/docs/OpenApiConfig.java
@@ -3,7 +3,6 @@ package com.synapse.backend.docs;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 
 import org.springframework.context.annotation.Bean;
@@ -23,7 +22,6 @@ public class OpenApiConfig {
                 .addSecuritySchemes("bearerAuth", new SecurityScheme()
                     .type(SecurityScheme.Type.HTTP)
                     .scheme("bearer")
-                    .bearerFormat("JWT")))
-            .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
+                    .bearerFormat("JWT")));
     }
 }
