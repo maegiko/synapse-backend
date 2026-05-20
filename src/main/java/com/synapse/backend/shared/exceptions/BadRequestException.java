8f6b0a74b9955342c7f8ae8f855aab049da3e09f
Kenneth Koon <kennethk.dev@gmail.com>
2026-05-20 19:55:52 +1000

    fix: Fixed class not being abstract

--- src/main/java/com/synapse/backend/shared/exceptions/BadRequestException.java
+++ src/main/java/com/synapse/backend/shared/exceptions/BadRequestException.java
@@ -1,6 +1,6 @@
 package com.synapse.backend.shared.exceptions;
 
-public class BadRequestException extends RuntimeException {
+public abstract class BadRequestException extends RuntimeException {
 
     public BadRequestException(String message) {
         super(message);
