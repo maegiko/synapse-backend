759bb73da3d588528541d2a0bc0b99c0265b9c32
Kenneth Koon <kennethk.dev@gmail.com>
2026-05-20 21:11:06 +1000

    feat: Created interface for LLM clients

---
+++ src/main/java/com/synapse/backend/ai/clients/LLMClient.java
@@ -0,0 +1,14 @@
+package com.synapse.backend.ai.clients;
+
+import com.synapse.backend.ai.clients.dto.LLMRequest;
+
+public interface LLMClient {
+
+    /**
+     * Makes an API call to an LLM and returns the output.
+     * @param llmRequest the model and prompt for the request.
+     * @return the string output of the LLM.
+     */
+    public String generate(LLMRequest llmRequest);
+
+}
