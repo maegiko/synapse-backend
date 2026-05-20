7dbbe4281e9a15b799c8c2c2955645652f6473ea
Kenneth Koon <kennethk.dev@gmail.com>
2026-05-20 21:10:43 +1000

    feat: Implemented Client for making Google AI Studio API calls

---
+++ src/main/java/com/synapse/backend/ai/clients/gemini/GeminiClient.java
@@ -0,0 +1,62 @@
+package com.synapse.backend.ai.clients.gemini;
+
+import java.util.List;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestClientException;
+
+import com.synapse.backend.ai.clients.LLMClient;
+import com.synapse.backend.ai.clients.dto.LLMRequest;
+import com.synapse.backend.ai.clients.gemini.dto.GeminiContent;
+import com.synapse.backend.ai.clients.gemini.dto.GeminiPart;
+import com.synapse.backend.ai.clients.gemini.dto.GeminiRequest;
+import com.synapse.backend.ai.clients.gemini.dto.GeminiResponse;
+import com.synapse.backend.ai.config.GeminiProperties;
+import com.synapse.backend.ai.exceptions.LLMProviderException;
+
+@Component
+public class GeminiClient implements LLMClient {
+    private final RestClient restClient;
+    private final GeminiProperties geminiProperties;
+
+    public GeminiClient(RestClient restClient, GeminiProperties geminiProperties) {
+        this.restClient = restClient;
+        this.geminiProperties = geminiProperties;
+    }
+
+    @Override
+    public String generate(LLMRequest llmRequest) {
+        GeminiRequest body = new GeminiRequest(
+            List.of(
+                new GeminiContent(
+                    List.of(
+                        new GeminiPart(llmRequest.prompt())
+                    )
+                )
+            )
+        );
+
+        try {
+            GeminiResponse res = restClient.post()
+            .uri("https://generativelanguage.googleapis.com/v1beta/models/{model}:generateContent", llmRequest.model())
+            .header("x-goog-api-key", geminiProperties.apiKey())
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(body)
+            .retrieve()
+            .body(GeminiResponse.class);
+
+            return res
+                .candidates()
+                .get(0)
+                .content()
+                .parts()
+                .get(0)
+                .text();
+        } catch (RestClientException | NullPointerException | IndexOutOfBoundsException e) {
+            throw new LLMProviderException("Failed to get a valid response from the LLM provider");
+        }
+    }
+
+}
