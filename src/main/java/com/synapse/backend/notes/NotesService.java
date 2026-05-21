3c5edcaff134fde8205634e07a2990aece88d552
Kenneth Koon <kennethk.dev@gmail.com>
2026-05-21 16:47:32 +1000

    refactor: Removed unnecessary layer of abstraction, moving LLMClient calls to NotesService

--- src/main/java/com/synapse/backend/notes/NotesService.java
+++ src/main/java/com/synapse/backend/notes/NotesService.java
@@ -3,23 +3,39 @@ package com.synapse.backend.notes;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.synapse.backend.ai.LLMService;
+import com.synapse.backend.ai.clients.LLMClient;
+import com.synapse.backend.ai.clients.dto.LLMRequest;
+import com.synapse.backend.ai.exceptions.LLMResponseParsingException;
+import com.synapse.backend.ai.prompts.NoteSummaryPromptFactory;
 import com.synapse.backend.notes.dto.NoteSummaryResponse;
 import com.synapse.backend.notes.exceptions.InvalidFileException;
 import com.synapse.backend.shared.files.FileParsingService;
 
+import tools.jackson.core.JacksonException;
+import tools.jackson.databind.ObjectMapper;
+
 @Service
 public class NotesService {
-    private final LLMService llmService;
     private final FileParsingService fileParsingService;
+    private final LLMClient llmClient;
+    private final ObjectMapper objectMapper;
+    private final NoteSummaryPromptFactory promptFactory;
 
-    public NotesService(LLMService llmService, FileParsingService fileParsingService) {
-        this.llmService = llmService;
+    public NotesService(
+        FileParsingService fileParsingService,
+        LLMClient llmClient,
+        ObjectMapper objectMapper,
+        NoteSummaryPromptFactory promptFactory
+    ) {
         this.fileParsingService = fileParsingService;
+        this.llmClient = llmClient;
+        this.objectMapper = objectMapper;
+        this.promptFactory = promptFactory;
     }
 
     /**
      * Returns the summarised notes.
+     *
      * @param file the file to be summarised.
      * @return a structured summary of the file.
      */
@@ -28,7 +44,19 @@ public class NotesService {
             throw new InvalidFileException("The file cannot be empty.");
 
         String extractedText = fileParsingService.extractText(file);
-        return llmService.summariseNotes(extractedText);
+
+        LLMRequest req = new LLMRequest(
+            "meta-llama/llama-4-scout-17b-16e-instruct",
+            promptFactory.createNoteSummarySystemPrompt(),
+            promptFactory.createNoteSummaryUserPrompt(extractedText)
+        );
+        String res = llmClient.generate(req);
+
+        try {
+            return objectMapper.readValue(res, NoteSummaryResponse.class);
+        } catch (JacksonException e) {
+            throw new LLMResponseParsingException("Failed to parse LLM response");
+        }
     }
 
 }
