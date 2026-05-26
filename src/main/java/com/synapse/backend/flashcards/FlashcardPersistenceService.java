c766a932bb8b4d7660214d385e7c13fc954bc16f
Kenneth Koon <kennethk.dev@gmail.com>
2026-05-26 18:43:55 +1000

    feat: Created persistence service and implemented saving flashcards to db

---
+++ src/main/java/com/synapse/backend/flashcards/FlashcardPersistenceService.java
@@ -0,0 +1,47 @@
+package com.synapse.backend.flashcards;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.synapse.backend.flashcards.dto.FlashcardGenerateResponse;
+import com.synapse.backend.flashcards.entities.Flashcard;
+import com.synapse.backend.flashcards.entities.FlashcardDeck;
+import com.synapse.backend.flashcards.repositories.FlashcardDeckRepository;
+import com.synapse.backend.flashcards.repositories.FlashcardRepository;
+import com.synapse.backend.notes.dto.NoteSummaryResponse;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class FlashcardPersistenceService {
+    private final FlashcardDeckRepository flashcardDeckRepository;
+    private final FlashcardRepository flashcardRepository;
+
+    public FlashcardPersistenceService(
+        FlashcardDeckRepository flashcardDeckRepository,
+        FlashcardRepository flashcardRepository
+    ) {
+        this.flashcardDeckRepository = flashcardDeckRepository;
+        this.flashcardRepository = flashcardRepository;
+    }
+
+    @Transactional
+    public void saveFlashcardFromNote(List<FlashcardGenerateResponse> flashcards, Long userId, NoteSummaryResponse note) {
+        if (flashcards == null) return;
+
+        FlashcardDeck flashcardDeck = new FlashcardDeck(userId, note.id(), note.title(), "NOTE");
+        FlashcardDeck newFlashcardDeck = flashcardDeckRepository.save(flashcardDeck);
+
+        List<Flashcard> newFlashcards = new ArrayList<>();
+
+        for (int i = 0; i < flashcards.size(); i++) {
+            FlashcardGenerateResponse flashcard = flashcards.get(i);
+            newFlashcards.add(new Flashcard(newFlashcardDeck.getId(), flashcard.title(), flashcard.answer(), i));
+        }
+
+        flashcardRepository.saveAll(newFlashcards);
+    }
+
+}
