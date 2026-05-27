de16f9c90aa234c636825a89dc100fc4dfafd30e
Kenneth Koon <kennethk.dev@gmail.com>
2026-05-27 23:58:10 +1000

    refactor: Renamed to SingleDeckResponse

--- src/main/java/com/synapse/backend/flashcards/dto/list/SingleDeckResponse.java
+++ src/main/java/com/synapse/backend/flashcards/dto/list/SingleDeckResponse.java
@@ -3,7 +3,7 @@ package com.synapse.backend.flashcards.dto.list;
 import java.util.List;
 import java.util.UUID;
 
-public record DeckListResponse(
+public record SingleDeckResponse(
     UUID deckId,
     List<FlashcardWithIdResponse> flashcards
 ) {}
