b97b3a63fd7f53052b15a8fc9c8f692a0ed26267
Kenneth Koon <kennethk.dev@gmail.com>
2026-05-31 15:00:12 +1000

    fix: Fixed exception extending incorrect base exception

--- src/main/java/com/synapse/backend/quiz/exceptions/QuizNotFound.java
+++ src/main/java/com/synapse/backend/quiz/exceptions/QuizNotFound.java
@@ -1,8 +1,8 @@
 package com.synapse.backend.quiz.exceptions;
 
-import com.synapse.backend.shared.exceptions.BadRequestException;
+import com.synapse.backend.shared.exceptions.NotFoundException;
 
-public class QuizNotFound extends BadRequestException {
+public class QuizNotFound extends NotFoundException {
 
     public QuizNotFound(String message) {
         super(message);
