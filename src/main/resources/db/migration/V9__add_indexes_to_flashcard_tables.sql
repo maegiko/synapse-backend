e4fe755f9907787463a4479b5cf25541d348f663
Kenneth Koon <kennethk.dev@gmail.com>
2026-06-01 14:27:36 +1000

    feat: Added indexes to flashcard tables

---
+++ src/main/resources/db/migration/V9__add_indexes_to_flashcard_tables.sql
@@ -0,0 +1,3 @@
+CREATE INDEX idx_flashcard_deck_user_id ON flashcard_deck(user_id);
+CREATE INDEX idx_flashcard_deck_note_id ON flashcard_deck(note_id);
+CREATE INDEX idx_flashcard_deck_id ON flashcard(deck_id);
