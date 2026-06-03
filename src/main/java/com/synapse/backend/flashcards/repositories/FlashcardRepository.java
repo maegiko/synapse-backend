058ec3fa19c15a0ad24860b974b18fdcc244a25b
Kenneth Koon <kennethk.dev@gmail.com>
2026-06-03 23:26:38 +1000

    feat: Added explicit method for getting the max position field of a row by deckId

--- src/main/java/com/synapse/backend/flashcards/repositories/FlashcardRepository.java
+++ src/main/java/com/synapse/backend/flashcards/repositories/FlashcardRepository.java
@@ -1,8 +1,11 @@
 package com.synapse.backend.flashcards.repositories;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.synapse.backend.flashcards.entities.Flashcard;
 
@@ -11,5 +14,8 @@ public interface FlashcardRepository extends JpaRepository<Flashcard, Long> {
     List<Flashcard> findByDeckIdOrderByPositionAsc(Long deckId);
 
     List<Flashcard> findByDeckIdInOrderByDeckIdAscPositionAsc(List<Long> deckIds);
+
+    @Query("SELECT MAX(f.position) FROM Flashcard f WHERE f.deckId = :deckId")
+    Optional<Integer> findMaxPositionByDeckId(@Param("deckId") Long deckId);
 
 }
