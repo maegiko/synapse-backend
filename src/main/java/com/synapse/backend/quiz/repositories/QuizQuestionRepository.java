724a5222cb5f3c3f68e0b14ee710957c84a6ebdc
Kenneth Koon <kennethk.dev@gmail.com>
2026-06-08 18:14:22 +1000

    feat: Created method to get max position value of quiz questions

--- src/main/java/com/synapse/backend/quiz/repositories/QuizQuestionRepository.java
+++ src/main/java/com/synapse/backend/quiz/repositories/QuizQuestionRepository.java
@@ -1,8 +1,11 @@
 package com.synapse.backend.quiz.repositories;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.synapse.backend.quiz.entities.QuizQuestion;
 
@@ -11,5 +14,8 @@ public interface QuizQuestionRepository extends JpaRepository<QuizQuestion, Long
     List<QuizQuestion> findByQuizIdOrderByPositionAsc(Long quizId);
 
     List<QuizQuestion> findByQuizIdInOrderByQuizIdAscPositionAsc(List<Long> quizIds);
+
+    @Query("SELECT MAX(q.position) FROM QuizQuestion q WHERE q.quizId =:quizId")
+    Optional<Integer> findMaxPositionByQuizId(@Param("quizId") Long quizId);
 
 }
