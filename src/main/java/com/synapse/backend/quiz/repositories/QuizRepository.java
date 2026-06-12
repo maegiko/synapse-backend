00ce22516110ad23eb40ba7ef02c3e2876990553
Kenneth Koon <kennethk.dev@gmail.com>
2026-06-12 19:42:16 +1000

    feat: Added query to update difficulty column

--- src/main/java/com/synapse/backend/quiz/repositories/QuizRepository.java
+++ src/main/java/com/synapse/backend/quiz/repositories/QuizRepository.java
@@ -26,4 +26,13 @@ public interface QuizRepository extends JpaRepository<Quiz, Long> {
     """)
     long updateUpdatedAtById(@Param("quizId") Long quizId);
 
+    @Modifying
+    @Query("""
+        UPDATE Quiz q
+        SET q.difficulty = :difficulty,
+            q.updatedAt = CURRENT_TIMESTAMP
+        WHERE q.id = :quizId
+    """)
+    long updateDifficultyById(@Param("quizId") Long quizId, @Param("difficulty") Integer difficulty);
+
 }
